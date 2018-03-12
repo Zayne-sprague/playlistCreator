97a6f9ff9c56aae87e88c80c7fabb19609e063cf
zayne sprague <zayne_sprague@yahoo.com>
2018-03-11 20:51:26 -0500

    Initial Commit

---
+++ PlaylistCreator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlaylistCreator
+//
+//  Created by zayne sprague on 3/11/18.
+//  Copyright © 2018 ZRS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
